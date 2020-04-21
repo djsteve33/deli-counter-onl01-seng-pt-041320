125e95de07ab16b062d62510f21646ff322be794
Steve Womble <swomble@aol.com>
2020-04-21 22:51:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,11 @@
 def line(array)
-  
+   if array.count == 0
+      puts "The line is currently empty."
+   else
+      response = "The line is currently:"
+      array.each_with_index do |name, index|
+         response += " #{index+1}. #{name}" 
+   end
+   puts response
+   end
 end
